a5070e9f116769b7449ecb5dfdf33d6c4fdceb1f
Berserker1523 <berserker1523@hotmail.com>
2019-09-29 19:35:24 -0500

    aaa

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -78,7 +78,7 @@ public class Cliente {
 		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
 			totalRead += read;
 			remaining -= read;
-			//System.out.println("read " + totalRead + " bytes.");
+			System.out.println("read " + totalRead + " bytes.");
 			fos.write(buffer, 0, read);
 		}
 		
