89a5e88cbfa94f3e96b0e17c009ade8b86e0904f
Berserker1523 <berserker1523@hotmail.com>
2019-09-29 20:09:56 -0500

    hash implementado

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -4,16 +4,19 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import Servidor.Server;
 
 public class Cliente {
 
-	private final static String SERVER_ADDRESS = "54.87.204.180";
+	private final static String SERVER_ADDRESS = "localhost";
 	private static DataOutputStream outToServer = null;
 	private static BufferedReader inFromServer = null;
 
@@ -63,7 +66,7 @@ public class Cliente {
 	}
 	
 	
-	private static void saveFile(Socket clientSock, String nameFile, String size) throws IOException {
+	private static void saveFile(Socket clientSock, String nameFile, String size) throws IOException, NoSuchAlgorithmException {
 		
 		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
 		FileOutputStream fos = new FileOutputStream( new File("./downloads/"+ nameFile));
@@ -85,10 +88,53 @@ public class Cliente {
 		
 		System.out.println("DONE File Transfer ");
 		
+		String fromServer = inFromServer.readLine();
+		System.out.println("Server hash: " + fromServer);
+		
+		File receipt = new File("./downloads/"+ nameFile);
+		//Use MD5 algorithm
+		MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+		
+		//Get the checksum
+		String checksum = getFileChecksum(md5Digest, receipt);
+		System.out.println("Client hash: " + checksum);
+		
 		outToServer.writeBytes("X" + '\n');
 		System.out.println("TO SERVER End");
 
 		fos.close();
 		dis.close();
+	}
+	
+	private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+	{
+	    //Get file input stream for reading the file content
+	    FileInputStream fis = new FileInputStream(file);
+	     
+	    //Create byte array to read data in chunks
+	    byte[] byteArray = new byte[1024];
+	    int bytesCount = 0;
+	      
+	    //Read file data and update in message digest
+	    while ((bytesCount = fis.read(byteArray)) != -1) {
+	        digest.update(byteArray, 0, bytesCount);
+	    };
+	     
+	    //close the stream; We don't need it now.
+	    fis.close();
+	     
+	    //Get the hash's bytes
+	    byte[] bytes = digest.digest();
+	     
+	    //This bytes[] has bytes in decimal format;
+	    //Convert it to hexadecimal format
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i< bytes.length ;i++)
+	    {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+	     
+	    //return complete hash
+	   return sb.toString();
 	}
 }
--- src/Servidor/ServerThread.java
+++ src/Servidor/ServerThread.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.MessageDigest;
 
 public class ServerThread extends Thread {
 	private int id;
@@ -63,6 +64,17 @@ public class ServerThread extends Thread {
 				}
 				else if(sendFile == true){
 					writeFile2Client(Server.fileToSend);
+					
+					File sent = new File(Server.fileToSend.getAbsolutePath());
+					//Use MD5 algorithm
+					MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+					
+					//Get the checksum
+					String checksum = getFileChecksum(md5Digest, sent);
+					System.out.println("Server hash: " + checksum);
+					
+					outToClient.writeBytes(checksum + "\n");
+					
 					clientSentence = inFromClient.readLine();
 					if (clientSentence == null) {
 						break;
@@ -120,5 +132,37 @@ public class ServerThread extends Thread {
 
 	private void log(String wat) {
 		System.out.println(id + ": " + wat);
+	}
+	
+	private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+	{
+	    //Get file input stream for reading the file content
+	    FileInputStream fis = new FileInputStream(file);
+	     
+	    //Create byte array to read data in chunks
+	    byte[] byteArray = new byte[1024];
+	    int bytesCount = 0;
+	      
+	    //Read file data and update in message digest
+	    while ((bytesCount = fis.read(byteArray)) != -1) {
+	        digest.update(byteArray, 0, bytesCount);
+	    };
+	     
+	    //close the stream; We don't need it now.
+	    fis.close();
+	     
+	    //Get the hash's bytes
+	    byte[] bytes = digest.digest();
+	     
+	    //This bytes[] has bytes in decimal format;
+	    //Convert it to hexadecimal format
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i< bytes.length ;i++)
+	    {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+	     
+	    //return complete hash
+	   return sb.toString();
 	}
 }
